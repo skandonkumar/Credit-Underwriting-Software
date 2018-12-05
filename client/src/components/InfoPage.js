9941f3c04bcba1b124ab1d8f67c696cb4bc938e2
inirav51 <40270335+inirav51@users.noreply.github.com>
2018-12-04 19:48:41 -0500

    Update InfoPage.js

--- client/src/components/InfoPage.js
+++ client/src/components/InfoPage.js
@@ -6,15 +6,18 @@ class InfoPage extends Component{
     render(){
         return(
             <div className="row">
-                <div className="col s10 m5">
-                    <div className="card-panel teal lighten-4">
-                        <span className="white-text">
-                            Hello
-                        </span>
-                    </div>
-                </div>
+            <div className="col s10 m5">
+            <div className="card-panel teal lighten-4">
+            <span className="white-text">
+            <li>Login with your Google accout to get started. </li>
+            <li>Create New Form: Get started with new form and fill out the Financial data get scores. </li>
+            <li>Update Form: Enter Form Id to retrieve and upadate the existing form. </li>
+            <li>Export as xlxs: To get get a Excel file of the existing form. </li>
+            </span>
             </div>
-        )
+            </div>
+            </div>
+    )
     }
 }
 
